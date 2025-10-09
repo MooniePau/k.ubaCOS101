a849fa1793c9fc7d3e9027926928160eb3ebc9a5
MooniePau <kenechukwu-oluwanifemi.uba@pau.edu.ng>
2025-10-09 14:27:24 +0100

    (Did compound interest, and printed it)

--- week-2/project_1.rs
+++ week-2/project_1.rs
@@ -1,1 +1,20 @@
-fn ma
+fn main() {
+
+	//state variables
+
+	let p:f64 = 520000000.0;
+	let n:f64 = 5.0;
+	let r:f64 = 10.0; 
+
+
+	//calculate compund interest
+
+	let a = p * (1.0 + (r / 100.0)) * n;
+	let ci = a - p;
+
+
+	//print compound interest
+
+	println!("Compound Interest is {}", ci);
+	
+}
