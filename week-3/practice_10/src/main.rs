9af65eb8daf29d32c012b5b5883c9e887a1b47c2
MooniePau <kenechukwu-oluwanifemi.uba@pau.edu.ng>
2025-10-15 13:45:28 +0100

    (debugged errors)

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    //addition
+    let sum = 5550 + 7310;
+    print!("The sum of 5550 and 7310 = {}", sum);
+
+    //subtraction
+    let difference:f32 = 95.5 -4.3;
+    println!("the difference of 95.5 and 4.3 = {}", difference);
+
+    //multiplication
+    let product:u32 = 4 * 30;
+    println!("the multiplication of 4 and 30 is {}", product);
+
+    //division
+    let quotient:f32 = 56.7 / 32.2;
+    println!("the division of 56.7 and 32.2 = {}", quotient);
+
+    //remainder
+    let remainder = 43 % 5;
+    println!("the remainder of 43 and 5 = {}", remainder);
+}
---
+++ week-3/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\Morayo\Documents\k.ubaCOS101\week-3\practice_10\target\debug\practice_10.exe: C:\Users\Morayo\Documents\k.ubaCOS101\week-3\practice_10\src\main.rs
