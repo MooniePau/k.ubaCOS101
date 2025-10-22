a8e5959932acc31aa4a9a6a1daca4250dba6e4c8
MooniePau <kenechukwu-oluwanifemi.uba@pau.edu.ng>
2025-10-22 13:10:37 +0100

    (student information management system)

---
+++ week_4/practice_1/src/main.rs
@@ -0,0 +1,24 @@
+
+use std::io;
+
+fn main() {
+    println!("\nStudent Information Management System. ");
+
+    //input name
+    println!("\n Please Enter your name: ");
+    let mut name = String::new();
+    io::stdin()
+    .read_line(&mut name)
+    .expect("Failed to read inout");
+    println!("Your name is: {}", name);
+
+    //input age
+    println!("\n Enter your age:");
+    let mut age = String::new();
+    io::stdin()
+    .read_line(&mut age)
+    .expect("Failed to read input");
+    //change string to integer
+    let age:i32 = age.trim().parse().expect("Input ont an integer");
+    println!("Your age is: {}", age);
+}
---
+++ week_4/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\Morayo\Documents\k.ubaCOS101\week_4\practice_1\target\debug\practice_1.exe: C:\Users\Morayo\Documents\k.ubaCOS101\week_4\practice_1\src\main.rs
