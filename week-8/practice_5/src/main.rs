825aa14aef9487f289b0a861c4cfb603100ab9e1
MooniePau <kenechukwu-oluwanifemi.uba@pau.edu.ng>
2025-11-19 13:49:50 +0100

    pushing new elements into a vector object and collecting user input

---
+++ week-8/practice_5/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    //create an empty vector "city"
+    let mut city:Vec<String> = Vec::new();
+
+    //print city vector
+    println!("The city vector has elements {}", city.len());
+
+    //push new elements into city
+    let mut input1 = String::new();
+    println!("How many cities do you want enter?");
+    std::io::stdin().read_line(&mut input1).expect("Failed to read input");
+    let city_num:i32 = input1.trim().parse().expect("Invalid input");
+
+    //iterating 
+    for count in 0..city_num 
+    {
+        let mut input2 = String::new();
+        println!("Enter city {}",count + 1);
+        std::io::stdin().read_line(&mut input2).expect("Failed to read input");
+        let new_city:String = input2.trim().parse().expect("Invalid input");
+        city.push(new_city);
+    }
+
+    println!("Your preferred cities are: ");
+    let mut count = 1;
+
+    //loop to iterate through elements
+    for i in city 
+    {
+        println!("{} {}", count, i);
+        count += 1;
+    }
+}
---
+++ week-8/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\Morayo\Documents\k.ubaCOS101\week-8\practice_5\target\debug\practice_5.exe: C:\Users\Morayo\Documents\k.ubaCOS101\week-8\practice_5\src\main.rs
