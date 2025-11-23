4e5f071d7fe654fa84675e50eab5f4dbad5284d3
MooniePau <kenechukwu-oluwanifemi.uba@pau.edu.ng>
2025-11-23 08:54:43 +0100

    adding elements into a vector and using iteration

--- week-8/practice_5/src/main.rs
+++ week-8/practice_5/src/main.rs
@@ -8,21 +8,28 @@ fn main() {
     //push new elements into city
     let mut input1 = String::new();
     println!("How many cities do you want enter?");
-    std::io::stdin().read_line(&mut input1).expect("Failed to read input");
+    std::io::stdin()
+    .read_line(&mut input1)
+    .expect("Failed to read input");
+    
     let city_num:i32 = input1.trim().parse().expect("Invalid input");
 
     //iterating 
+    let mut count = 1;
+
     for count in 0..city_num 
     {
         let mut input2 = String::new();
         println!("Enter city {}",count + 1);
-        std::io::stdin().read_line(&mut input2).expect("Failed to read input");
+        std::io::stdin()
+        .read_line(&mut input2)
+        .expect("Failed to read input");
+
         let new_city:String = input2.trim().parse().expect("Invalid input");
         city.push(new_city);
     }
 
     println!("Your preferred cities are: ");
-    let mut count = 1;
 
     //loop to iterate through elements
     for i in city 
