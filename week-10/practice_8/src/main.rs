d638d59c636eaf23928d247d74bc1364c30c655f
MooniePau <kenechukwu-oluwanifemi.uba@pau.edu.ng>
2025-12-03 13:46:01 +0100

    declaring and initializing a struct, inputing values in and passing it into a function

---
+++ week-10/practice_8/src/main.rs
@@ -0,0 +1,32 @@
+struct Employee {
+    ceo:String,
+    company:String,
+    age:u32
+}
+
+fn main() {
+    //initializing a struct
+
+    let emp1 = Employee {
+        company:String::from("Microsoft Corporation"),
+        ceo:String::from("Satya Nadella"),
+        age:56
+    };
+
+    let emp2 = Employee {
+        company:String::from("Google Inc."),
+        ceo:String::from("Sundai Pichai"),
+        age:51
+    };
+
+    //passing emp1 and emp2 to display function
+
+    display(emp1);
+    display(emp2);
+}
+
+//fetch values of specific structure fields using the
+//operator and print
+fn display(emp:Employee) {
+    println!("Name is: {} company is {} age is {}", emp.ceo, emp.company,emp.age);
+}
