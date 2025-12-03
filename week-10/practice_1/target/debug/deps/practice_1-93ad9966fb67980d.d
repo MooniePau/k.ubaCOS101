d05a6178e5bed43eab262b97949c24562ed2fdd2
MooniePau <kenechukwu-oluwanifemi.uba@pau.edu.ng>
2025-12-03 13:11:08 +0100

    transferred ownership of one variable to another

---
+++ week-10/practice_1/target/debug/deps/practice_1-93ad9966fb67980d.d
@@ -0,0 +1,5 @@
+C:\Users\Morayo\Documents\k.ubaCOS101\week-10\practice_1\target\debug\deps\practice_1-93ad9966fb67980d.d: src\main.rs
+
+C:\Users\Morayo\Documents\k.ubaCOS101\week-10\practice_1\target\debug\deps\libpractice_1-93ad9966fb67980d.rmeta: src\main.rs
+
+src\main.rs:
